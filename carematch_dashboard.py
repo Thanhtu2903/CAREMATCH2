44af60aed91b6260ba0c1594d37752020c16a530
Thanhtu2903 <60025986+Thanhtu2903@users.noreply.github.com>
2025-09-30 15:11:01 -0400

    Update carematch_dashboard.py

--- carematch_dashboard.py
+++ carematch_dashboard.py
@@ -8,7 +8,18 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-
+import yake
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+from pathlib import Path
+from scipy.sparse import hstack
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import MiniBatchKMeans       # works with sparse
+from sklearn.decomposition import TruncatedSVD
+from pathlib import Path
+import streamlit as st
 # === Load Dataset ===
 carematch = pd.read_csv("carematch_requests.csv")
 st.markdown(""" ***GROUP 4***: TU PHAM & MINH NGUYEN""")
